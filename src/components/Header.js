fa6b2c365f8b99a6c4399b5593caef0b11fa966d
noahlinn <n.linn95@gmail.com>
2021-06-03 14:15:36 -0400

    add linkedin

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import gmail from '../assets/gmail.png'
 import git from '../assets/git.png'
 import resume from '../assets/resume.png'
 import NLinn_Resume from '../assets/NLinn_Resume.pdf'
-
+import linkedin from '../assets/linkedin.png'
 
 
 const Header = () => {
@@ -16,13 +16,16 @@ const Header = () => {
                 </div>
                 <span className="icons">
                     <a target="_blank" href="https://github.com/noahlinn" title="GitHub">
-                        <img className="contact" id="git-icon" src={git} />
+                        <img className="contact" src={git} />
                     </a>
                     <a target="_blank" href="mailto:n.linn95@gmail.com?subject=" title="Email">
-                        <img className="contact" id="gmail-icon" src={gmail} />
+                        <img className="contact" src={gmail} />
                     </a>
                     <a target="_blank" href={NLinn_Resume} title="Resume">
                         <img className="contact" id="resume-icon" src={resume} />
+                    </a>
+                    <a target="_blank" href="https://www.linkedin.com/in/noah-linn/" title="Linkedin">
+                        <img className="contact" src={linkedin} />
                     </a>
                 </span>
             </div>
